c94e941252f758761b781838180301eead1fbb88
Dalton <Daltonjavery@gmail.com>
2021-10-16 19:01:38 -0500

    updated timer settings, added set and dict

--- TDSynthesis.py
+++ TDSynthesis.py
@@ -3,12 +3,12 @@ import time
 
 ###################
 
-url = "https://api.opensea.io/api/v1/events?only_opensea=false&offset=0&limit=100&occurred_before=2021-10-16T20%3A35%3A44.511Z&occurred_after=2021-10-16T20%3A35%3A43.511Z"
+url = "https://api.opensea.io/api/v1/events?only_opensea=false&offset=0&limit=1&occurred_after=2021-10-16T20%3A35%3A43.511Z"
 headers = {"Accept": "application/json"}
 seenTokens = []
 
 ######
-keyQueue = []
+keyQueue = set()
 minuteDict = {}
 ######
 
@@ -21,20 +21,30 @@ while(1):
 	for x in data['asset_events']:
 		if (x['asset']['asset_contract']['symbol']!='OPENSTORE' and (x['asset']['token_id'] not in seenTokens) and x['asset']['asset_contract']['schema_name']=='ERC721'):
 			if (timer<60):
+				pass
 				#add to 1 minute column
 			if (timer<240):
+				pass
 				#add to 5 minutes column
 			if (timer<1200):
+				pass
 				#add to 20 minutes column
 			if (timer<2400):
+				pass
 				#add to 40 minutes column
 			if (timer<3600):
+				pass
 				#add to 1 hour column
 
 			print(x['asset']['asset_contract']['address']+" "+ x['asset']['token_id'] +" "+ x['asset']['asset_contract']['symbol'])
 			seenTokens.append(x['asset']['token_id'])
 			######
-			keyQueue.append(timer)
+			keyQueue.add(timer)
 			minuteDict[timer]={x['asset']['asset_contract']['symbol']}
 			######
+	timer+=1
+	print(keyQueue)
+	print(minuteDict)
+	print(timer)
 	time.sleep(1)
+
---
+++ test.py
@@ -0,0 +1,9 @@
+setTest = set()
+
+setTest.add(42)
+setTest.add(32)
+setTest.add(62)
+setTest.add(52)
+setTest.add(32)
+
+print(setTest)
